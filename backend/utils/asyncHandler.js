8ac1d53022a43bb878b90dd1c3fb46b49793df75
Tushar Sharma <ts817479@gmail.com>
2026-07-27 02:15:38 +0530

    lilu

--- backend/utils/asyncHandler.js
+++ backend/utils/asyncHandler.js
@@ -1,3 +1,3 @@
 // This is used for error handling in async functions and avoid writing try-catch blocks in every function
 export const asyncHandler = (fn) => (req,res,next) =>
-    Promise.resolve(fn(req,res,next)).catch(next);
+    Promise.resolve(fn(req,res,next)).catch(next); //promise wrap up the request handler and pass the error to the error handling middleware
